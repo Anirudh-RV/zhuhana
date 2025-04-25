0ae138264279fd3a3380e20950ab950b84dd70a4
anirudh-rv <anirudh.rv1234@gmail.com>
2025-04-24 20:43:07 -0700

    Service rename to Governor

--- Backend/governor/governor/asgi.py
+++ Backend/governor/governor/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for usermodel project.
+ASGI config for governor project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usermodel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'governor.settings')
 
 application = get_asgi_application()
--- Backend/governor/governor/wsgi.py
+++ Backend/governor/governor/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for usermodel project.
+WSGI config for governor project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usermodel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'governor.settings')
 
 application = get_wsgi_application()
