1344593c94273dab92647e98f43cd7544ff3b179
anirudh-rv <anirudh.rv1234@gmail.com>
2025-06-16 12:16:56 +0530

    basecode for the 4 pages set

---
+++ Frontend/frontend/src/signup/components/SignUpCard.tsx
@@ -0,0 +1,207 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import MuiCard from "@mui/material/Card";
+import Checkbox from "@mui/material/Checkbox";
+import Divider from "@mui/material/Divider";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import MuiLink from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import { GoogleIcon, FacebookIcon, SitemarkIcon } from "./CustomIcons";
+import { Link } from "react-router-dom";
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  boxShadow:
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  [theme.breakpoints.up("sm")]: {
+    width: "450px",
+  },
+  ...theme.applyStyles("dark", {
+    boxShadow:
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
+}));
+
+export default function SignInCard() {
+  const [emailError, setEmailError] = React.useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState("");
+  const [passwordError, setPasswordError] = React.useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState("");
+  const [nameError, setNameError] = React.useState(false);
+  const [nameErrorMessage, setNameErrorMessage] = React.useState("");
+
+  const validateInputs = () => {
+    const email = document.getElementById("email") as HTMLInputElement;
+    const password = document.getElementById("password") as HTMLInputElement;
+    const name = document.getElementById("name") as HTMLInputElement;
+
+    let isValid = true;
+
+    if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+      setEmailError(true);
+      setEmailErrorMessage("Please enter a valid email address.");
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage("");
+    }
+
+    if (!password.value || password.value.length < 6) {
+      setPasswordError(true);
+      setPasswordErrorMessage("Password must be at least 6 characters long.");
+      isValid = false;
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage("");
+    }
+
+    if (!name.value || name.value.length < 1) {
+      setNameError(true);
+      setNameErrorMessage("Name is required.");
+      isValid = false;
+    } else {
+      setNameError(false);
+      setNameErrorMessage("");
+    }
+
+    return isValid;
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    if (nameError || emailError || passwordError) {
+      event.preventDefault();
+      return;
+    }
+    const data = new FormData(event.currentTarget);
+    console.log({
+      name: data.get("name"),
+      lastName: data.get("lastName"),
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <Card variant="outlined">
+      <Box sx={{ display: { xs: "flex", md: "none" } }}>
+        <SitemarkIcon />
+      </Box>
+      <Typography
+        component="h1"
+        variant="h4"
+        sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
+      >
+        Create your account
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        noValidate
+        sx={{ display: "flex", flexDirection: "column", width: "100%", gap: 2 }}
+      >
+        <FormControl>
+          <FormLabel htmlFor="name">Full name</FormLabel>
+          <TextField
+            autoComplete="name"
+            name="name"
+            required
+            fullWidth
+            id="name"
+            placeholder="Jon Snow"
+            error={nameError}
+            helperText={nameErrorMessage}
+            color={nameError ? "error" : "primary"}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <TextField
+            error={emailError}
+            helperText={emailErrorMessage}
+            id="email"
+            type="email"
+            name="email"
+            placeholder="your@email.com"
+            autoComplete="email"
+            required
+            fullWidth
+            variant="outlined"
+            color={emailError ? "error" : "primary"}
+          />
+        </FormControl>
+        <FormControl>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <FormLabel htmlFor="password">Password</FormLabel>
+          </Box>
+          <TextField
+            error={passwordError}
+            helperText={passwordErrorMessage}
+            name="password"
+            placeholder="••••••"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            required
+            fullWidth
+            variant="outlined"
+            color={passwordError ? "error" : "primary"}
+          />
+        </FormControl>
+        <FormControlLabel
+          control={
+            <Checkbox
+              value="agreeToConditions"
+              color="primary"
+              defaultChecked
+            />
+          }
+          label={
+            <Box>
+              <Typography
+                variant="caption"
+                color="text.secondary"
+                sx={{ textAlign: "center" }}
+              >
+                Agree to our&nbsp;
+                <Link to="/sign-up" style={{ alignSelf: "center" }}>
+                  Terms of Service
+                </Link>
+                &nbsp;and&nbsp;
+                <Link to="/sign-up" style={{ alignSelf: "center" }}>
+                  Privacy Policy
+                </Link>
+                .
+              </Typography>
+            </Box>
+          }
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          onClick={validateInputs}
+        >
+          Sign in
+        </Button>
+        <Typography sx={{ textAlign: "center" }}>
+          Already have an account?{" "}
+          <span>
+            <Link to="/login" style={{ alignSelf: "center" }}>
+              Log in
+            </Link>
+          </span>
+        </Typography>
+      </Box>
+    </Card>
+  );
+}
