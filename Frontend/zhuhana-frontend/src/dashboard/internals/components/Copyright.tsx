ad0587f623d0c760f3e9c7b1baabe2e4f23a039a
anirudh-rv <anirudh.rv1234@gmail.com>
2025-07-20 13:11:15 +0530

    dashboard changes

--- Frontend/zhuhana-frontend/src/dashboard/internals/components/Copyright.tsx
+++ Frontend/zhuhana-frontend/src/dashboard/internals/components/Copyright.tsx
@@ -15,8 +15,8 @@ export default function Copyright(props: any) {
       ]}
     >
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Sitemark
+      <Link color="inherit" href="https://zhuhana.com/">
+        Zhuhana
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
